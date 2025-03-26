e28dd95fa94bb0442d503c3f92d17df0f60b29f7
Ekaterina3491 <katyatetra23@gmail.com>
2025-03-26 11:07:11 +0400

    new commit

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -3,5 +3,5 @@ def test_check_icon(browser):
       swag_labs_page = SwagLabs(browser)
       swag_labs_page.visit()
       assert swag_labs_page.exist_icon()
-      assert swag_labs_page.exist_username_field()
-      assert swag_labs_page.exist_password_field()
+      assert swag_labs_page.exist_name()
+      assert swag_labs_page.exist_password()
